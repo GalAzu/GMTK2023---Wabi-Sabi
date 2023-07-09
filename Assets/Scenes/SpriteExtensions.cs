de86d52568f014bc3b1c9ef46b52b51d6bd8f7dd
Gal Azulay <galazu@gmail.com>
2023-07-09 12:41:36 +0100

    ,

---
+++ Assets/Scenes/SpriteExtensions.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class SpriteExtensions
+{
+    public static IEnumerator FadeOutSprite(this Image sprite, float fadeDuration)
+    {
+        Color startColor = sprite.color;
+        Color targetColor = new Color(startColor.r, startColor.g, startColor.b, 0f);
+
+        float counter = 0f;
+
+        while (counter < fadeDuration)
+        {
+            float t = counter / fadeDuration;
+            Color lerpedColor = Color.Lerp(startColor, targetColor, t);
+            sprite.color = lerpedColor;
+
+            counter += Time.deltaTime;
+            yield return null;
+        }
+
+        sprite.color = targetColor;
+        sprite.gameObject.SetActive(false);
+    }
+}
