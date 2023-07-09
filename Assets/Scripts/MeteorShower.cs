3d52a779a951b4ecb4a79cdc08264a95fcb3aa3e
EkinSertkaya <108450130+EkinSertkaya@users.noreply.github.com>
2023-07-09 14:41:31 +0300

    meteor

--- Assets/Scripts/MeteorShower.cs
+++ Assets/Scripts/MeteorShower.cs
@@ -6,13 +6,36 @@ public class MeteorShower : MonoBehaviour
 {
     private Transform[] meteorArray;
 
+    private float meteorsIntensityTimerMax = 3f;
+    private float meteorsIntensityTimer = 0f;
+
+    private int intensityLevel = 1;
+
     private void Awake()
     {
         meteorArray = GetComponentsInChildren<Transform>();
 
         foreach(Transform transform in meteorArray)
         {
-            transform.gameObject.SetActive(false);
+            if(transform != this.transform)
+            {
+                transform.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        meteorsIntensityTimer += Time.deltaTime;
+
+        if(meteorsIntensityTimer >= meteorsIntensityTimerMax)
+        {
+            switch (intensityLevel)
+            {
+                case 1:
+                    meteorArray[Random.Range(0, meteorArray.Length)].gameObject.SetActive(true);
+                    break;
+            }
         }
     }
 
